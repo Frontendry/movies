84d07a9e8f276711b8cd997b405bc40e2e48d8c9
frontendry <brngatarwa@gmail.com>
2022-09-30 00:52:57 +0300

    movie intro design

--- src/components/general-components/movies/Movies.js
+++ src/components/general-components/movies/Movies.js
@@ -3,15 +3,17 @@ import Movie from "./Movie";
 
 const Movies = ({ moviesCollection, collectionTitle }) => {
   return (
-    <section>
-      <header>
-        <h2>{collectionTitle}</h2>
-      </header>
+    <section className="pb-16">
+      <div className="container mx-auto px-7">
+        <header className="mb-12">
+          <h2 className="text-white text-3xl">{collectionTitle}</h2>
+        </header>
 
-      <div className="">
-        {moviesCollection.map((movieData) => (
-          <Movie key={movieData.imdbID} movieData={movieData} />
-        ))}
+        <div className="grid grid-cols-4 gap-6">
+          {moviesCollection.map((movieData) => (
+            <Movie key={movieData.imdbID} movieData={movieData} />
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/components/general-components/movies/movie.js
+++ src/components/general-components/movies/movie.js
@@ -1,5 +1,29 @@
+// React Router
+import { Link } from "react-router-dom";
+
 const Movie = ({ movieData }) => {
-  return <div>{movieData.Title}</div>;
+  const { Title, Year, imdbID, Genre, PosterNew } = movieData;
+  return (
+    <article>
+      {PosterNew && (
+        <figure>
+          <Link to={`movie/${imdbID}`}>
+            <img src={PosterNew} alt={Title} />
+          </Link>
+        </figure>
+      )}
+
+      <div className="mt-2">
+        {Title && (
+          <h3 className="font-bold text-white mb-2">
+            <Link to={`movie/${imdbID}`}>{Title}</Link>{" "}
+          </h3>
+        )}
+
+        {Year && <span className="text-slate-500 block text-sm">{Year}</span>}
+      </div>
+    </article>
+  );
 };
 
 export default Movie;
