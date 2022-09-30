7939c03f205ec584efe113ba17dcbc3777d72eb5
frontendry <brngatarwa@gmail.com>
2022-09-30 05:56:27 +0300

    search root folder

--- src/components/sections/Search/index.js
+++ src/components/sections/Search/index.js
@@ -7,16 +7,18 @@ import SearchDropDown from "./searchDropDown";
 
 const SearchWrapper = () => {
   return (
-    <SearchContextProvider>
-      <section className="mt-10 mb-24">
-        <div className="container mx-auto px-7">
-          <div className="relative w-1/2 mx-auto">
-            <SearchForm />
-            <SearchDropDown />
+    <>
+      <SearchContextProvider>
+        <section className="mt-10 mb-24">
+          <div className="container mx-auto px-7">
+            <div className="relative w-1/2 mx-auto">
+              <SearchForm />
+              <SearchDropDown />
+            </div>
           </div>
-        </div>
-      </section>
-    </SearchContextProvider>
+        </section>
+      </SearchContextProvider>
+    </>
   );
 };
 
